8d13305976f18d88d5d603d520d5d3da2c55d928
Papillon6814 <41826375+Papillon6814@users.noreply.github.com>
2019-08-28 22:11:56 +0900

    axiosできたからコメントアウトで載せとく

--- mappp/map/App.js
+++ mappp/map/App.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
+import axios from 'axios';
 
 export default function App() {
   return (
     <MapView
-    style={{flex:1}}
+      style={{flex:1}}
     />
   );
 }
+
+/*
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'init'
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://elixir-http-coient.herokuapp.com/hello')
+         .then(res => {
+           this.setState({ text: res.data.text });
+         });
+  }
+
+  render() {
+    return (
+      <View style={{styles}}>
+        <Text>{this.state.text}</Text>
+      </View>
+    )
+  }
+}
+*/
 
 const styles = StyleSheet.create({
   container: {
